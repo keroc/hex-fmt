8b73c2e5c2808070cc0273a97de2aff23e48f5b7
Kévin Rocheteau <krocheteau@surgiris.fr>
2017-01-23 20:17:29 +0100

    Compute binary size on command
    - Add HexLine class to parse each file line
    - Use size method to get binary size

--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,85 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import {window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
+import {HexLine} from './hexline';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "ihex" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    // create a new byte counter
+    let hexDoc = new HexDocument();
 
-       var editor = vscode.window.activeTextEditor;
-       if (!editor) {
-           return;
-       }
-
-       var selection = editor.selection;
-       var text = editor.document.getText(selection);
-
-       // Display length
-       vscode.window.showInformationMessage('Selected characters: ' + text.length);
+    var disposable = commands.registerCommand('extension.sayHello', () => {
+        hexDoc.updateByteCount();
     });
 
+    // Add to a list of disposables which are disposed when this extension is deactivated.
+    context.subscriptions.push(hexDoc);
     context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+class HexDocument {
+
+    private _statusBarItem: StatusBarItem;
+
+    public updateByteCount() {
+
+        // Create as needed
+        if (!this._statusBarItem) {
+            this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+        }
+
+        // Get the current text editor
+        let editor = window.activeTextEditor;
+        if (!editor) {
+            this._statusBarItem.hide();
+            return;
+        }
+
+        let doc = editor.document;
+
+        // Only update status if an Hex file
+        if (doc.languageId === "hex") {
+            let binSize = this._updateDoc(doc);
+
+            // Update the status bar
+            if (binSize < 1024) {
+                this._statusBarItem.text = `${binSize} B`;
+            } else {
+                binSize /= 1024;
+                this._statusBarItem.text = `${binSize} KB`;
+            }
+            this._statusBarItem.show();
+        } else { 
+            this._statusBarItem.hide();
+        }
+    }
+
+    public _updateDoc(doc: TextDocument): number {
+
+        let docContent = doc.getText();
+
+        let hexLines = docContent.split("\n");
+        let docSize = 0;        
+        for (let i = 0; i < hexLines.length; i++) {
+            let hex = new HexLine(hexLines[i]);
+            docSize += hex.size()
+        }
+
+        return docSize;
+    }
+
+    dispose() {
+        this._statusBarItem.dispose();
+    }
 }
---
+++ src/hexline.ts
@@ -0,0 +1,64 @@
+import * as vscode from 'vscode';
+
+// Hex file types
+var TYPES = {
+    DATA: 0x00,
+    EOF: 0x01,
+    EXTSEGADDRESS: 0x02,
+    STARTSEGADDRESS: 0x03,
+    EXTLINADDRESS: 0x04,
+    STARTLINADDRESS: 0x05
+}
+
+// Header length = ':' + data length (2) + address (4) + type (2)
+var HEADERLENGTH = 9
+
+export class HexLine {
+    
+    startCode: string;
+    nbData: number;
+    address: number;
+    hexType: number;
+    data: number;
+    checksum: number;
+
+    correctHdr : boolean;
+    addOffset: number;
+    computedChk: number;
+
+    constructor(hexString: string) {
+        // Initialize variables
+        this.correctHdr = false;
+        this.computedChk = 0;
+        this.addOffset = 0;
+
+        // Check if the header is correct
+        if(hexString.length > HEADERLENGTH) {
+            this.startCode = hexString.charAt(0);
+            this.nbData = this.parseAndUpdateChk(hexString, 0);
+            this.address = 256 * this.parseAndUpdateChk(hexString, 1);
+            this.address += this.parseAndUpdateChk(hexString, 2);
+            this.hexType = this.parseAndUpdateChk(hexString, 3);
+
+            this.correctHdr = (this.startCode === ':' &&
+                this.nbData != NaN &&
+                this.address != NaN &&
+                this.hexType != NaN &&
+                TYPES.DATA <= this.hexType && this.hexType <= TYPES.STARTLINADDRESS);
+        }
+    }
+
+    private parseAndUpdateChk(hexString: string, byteId: number) {
+        let res = parseInt(hexString.substr(1 + 2*byteId, 2), 16);
+        this.computedChk += res;
+        return res;
+    }
+
+    public size() {
+        if(this.correctHdr && this.hexType === TYPES.DATA) {
+            return this.nbData;
+        }
+
+        return 0;
+    }
+}
